7a54f63eebd32ade73183760c65f47dfd665e370
Christopher Fryman <farfromguam@gmail.com>
2013-06-14 13:41:26 -0500

    new rails app, devise installed

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'devise'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '3.2.13'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Honymous::Application.config.secret_token = 'dbac76ee752cbd56c8aea384e71cc8bd1e4f98ef55adee706862a0790389171ae03f47549face044789824ce99340a810cf3fff7c4e0183f93224a9c3c100414'
