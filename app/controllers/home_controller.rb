bbb19036ccea4fe48fa6d28668b8a7e07ae4f837
Christopher Fryman <farfromguam@gmail.com>
2013-06-17 15:20:27 -0500

    added basic search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @profile_count = Profile.count
     @all_profiles = Profile.all
+    @profile_search = Profile.search(params[:search])
   end
 end
