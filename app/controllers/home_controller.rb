d534b937f64ef5b0f138843a6c640006300a7675
Christopher Fryman <farfromguam@gmail.com>
2013-06-26 11:08:19 -0500

    use X_FORWARDED_FOR

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @profile_count = Profile.all.length
     @search = Profile.search( params[:search] )
-    @ip = request.env['REMOTE_ADDR']
+    @ip = request.env['X_FORWARDED_FOR']
   end
 end
