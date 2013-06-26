8c5220f7010c18732e2be2e7a07d9b6489abbae6
Christopher Fryman <farfromguam@gmail.com>
2013-06-26 11:14:43 -0500

    HTTP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @profile_count = Profile.all.length
     @search = Profile.search( params[:search] )
-    @ip = request.env['X_FORWARDED_FOR']
+    @ip = request.env['HTTP_X_FORWARDED_FOR']
   end
 end
